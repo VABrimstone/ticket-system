5d1400d0604166dbdad81eb583c2a9e707a85a16
slawagn <slawagn@gmail.com>
2022-10-19 15:20:53 +0000

    set rbenv prefix

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,3 @@
-# config valid for current version and patch releases of Capistrano
 lock '~> 3.17.1'
 
 set :application, 'sem7-agile'
@@ -7,6 +6,7 @@ set :branch,      'main'
 
 set :deploy_to, '/home/deploy/apps/sem7-agile'
 
-# Файлы, которые лежат вне репозитория
 append :linked_files, '.env'
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads'
+
+set :rbenv_prefix, '/usr/bin/rbenv exec'
