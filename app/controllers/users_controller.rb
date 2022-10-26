cd0eb5ee27d40f7f79f89717c416ea895b9166d1
slawagn <slawagn@gmail.com>
2022-10-26 17:35:59 +0000

    implement editing user roles and statuses

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,24 @@ class UsersController < ApplicationController
   def index
     @users = User.ordered
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to users_path
+    else
+      render :edit, notice: 'Не удалось обновить данные'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:role, :status)
+  end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,17 +7,29 @@ module UsersHelper
     end
   end
 
-  def role_for_display(user)
-    case user.role
+  def role_for_display(role)
+    case role
     when 'regular' then 'Пользователь'
     when 'admin'   then 'Администратор'
     end
   end
 
-  def status_for_display(user)
-    case user.status
+  def status_for_display(status)
+    case status
     when 'active'   then 'Активен'
     when 'inactive' then 'Неактивен'
+    end
+  end
+
+  def roles_hash
+    User::roles.to_h do |role, db_representation|
+      [role_for_display(role), role]
+    end
+  end
+
+  def statuses_hash
+    User::statuses.to_h do |status, db_representation|
+      [status_for_display(status), status]
     end
   end
 end
