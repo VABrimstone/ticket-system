9109ceb76f8f32f5685771fa228e7a541b58029f
slawagn <slawagn@gmail.com>
2022-10-28 13:49:12 +0000

    set flash message and redirect on NotAuthorizedError

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
   include Pundit::Authorization
+  rescue_from Pundit::NotAuthorizedError do |exception|
+    flash[:alert] = "Недостаточно прав"
+    redirect_back(fallback_location: root_path)
+  end
 
   before_action :authenticate_user!
 end
