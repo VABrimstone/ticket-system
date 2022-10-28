d76aeb7ac84c1acff9cdb0dcfbd1ef769906a11e
slawagn <slawagn@gmail.com>
2022-10-28 14:01:34 +0000

    create Department model

---
+++ app/models/department.rb
@@ -0,0 +1,9 @@
+class Department < ApplicationRecord
+  enum :status, { active: 0, inactive: 1 }
+
+  validates :title,
+    presence:   true,
+    uniqueness: true
+
+  scope :ordered_alphabetically, ->{ order(:title, :asc) }
+end
