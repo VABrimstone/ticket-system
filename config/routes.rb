34b3f6250fa096e8259bad137184ad2dcda8e4c5
slawagn <slawagn@gmail.com>
2022-10-28 13:33:29 +0000

    remove unused routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'registrations'
   }
-  resources :users
+  resources :users, only: %i[index edit update]
 end
