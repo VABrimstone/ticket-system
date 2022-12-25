8008071c530fec2d745c183f20ca48489b1399db
slawagn <slawagn@gmail.com>
2022-12-25 19:00:43 +0000

    make seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+users =
+  User.create!([
+    {email: 'admin@mail', name: 'Администратор', role: 'admin', password: 'simplepass'},
+    {email: 'user@mail', name: 'Пользователь', password: 'simplepass'}
+  ])
+
+departments =
+  Department.create!([
+    {title: 'Финансово-экономическое управление'},
+    {title: 'Юридическй отдел'},
+    {title: 'Административное управление'},
+    {title: 'Служба безопасности'},
+    {title: 'Группа поддержки пользователей'},
+    {title: 'ЦО по БУНУ'},
+    {title: 'ОЦО по ИТ'},
+  ])
+
+
